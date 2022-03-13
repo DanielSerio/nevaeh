f4b0a9755bea2c652efc9e4d5a6da42d99977a68
DanielSerio <dserio.d@gmail.com>
2022-03-12 19:04:22 -0500

    theme and header styles added

--- components/Header/HeaderLogoArea.tsx
+++ components/Header/HeaderLogoArea.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function HeaderLogoArea() {
   return (
     <div className="header-logo-area">
-
+      <img src="./logo.png" alt="Nevaeh Logo" />
     </div>
   );
 }
